fd314a378ba6d85c00b02f790cdb19306870a8ab
Vakihito <victor.kamada.tomita@gmail.com>
2020-10-18 13:53:51 -0300

    pk

---
+++ test.py
@@ -0,0 +1,11 @@
+# Import classes from your brand new package
+from Animals.Mammals import Mammals
+from Animals.Birds import Birds
+ 
+# Create an object of Mammals class & call a method of it
+myMammal = Mammals()
+myMammal.printMembers()
+ 
+# Create an object of Birds class & call a method of it
+myBird = Birds()
+myBird.printMembers()
